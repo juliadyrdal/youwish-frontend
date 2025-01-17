bf7b9813473da63c5cdcbf5c671d3778ebb6f10d
juliadyrdal <juliadyrdal@protonmail.com>
2025-01-17 16:13:11 +0100

    Add Auth0 credentials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,38 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   modules: [
-    '@nuxtjs/tailwindcss', 
+    '@nuxtjs/tailwindcss',
     'nuxt-lodash',
     '@pinia/nuxt',
-    'pinia-plugin-persistedstate/nuxt'
+    'pinia-plugin-persistedstate/nuxt',
+    '@sidebase/nuxt-auth'
   ],
+  auth: {
+    globalAppMiddleware: true,
+    isEnabled: true,
+    originEnvKey: 'AUTH_ORIGIN',
+    baseURL: 'http://localhost:3001/api/auth',
+    providers: {
+      auth0: {
+        clientId: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        issuer: process.env.AUTH0_ISSUER,
+      }
+    }
+  },
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3000/api',
+      auth: {
+        origin: process.env.AUTH_ORIGIN,
+      },
+      auth0: {
+        clientId: process.env.AUTH0_CLIENT_ID,
+        domain: process.env.AUTH0_ISSUER,
+      }
+    },
+    private: {
+      auth0ClientSecret: process.env.AUTH0_CLIENT_SECRET
     }
   }
 })
---
+++ server/api/auth/[...].ts
@@ -0,0 +1,18 @@
+import Auth0Provider from "next-auth/providers/auth0";
+import { NuxtAuthHandler } from '#auth'
+
+export default NuxtAuthHandler({
+  // A secret string you define, to ensure correct encryption
+  secret: useRuntimeConfig().authSecret,
+  pages: {
+    signIn: '/'
+  },
+  providers: [
+    // @ts-expect-error Use .default here for it to work during SSR.
+    Auth0Provider.default({
+        clientId: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        issuer: process.env.AUTH0_ISSUER
+      })
+  ]
+})
